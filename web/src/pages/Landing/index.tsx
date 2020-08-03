d33d011e406c23328c49efcf16aba63e817b598e
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-03 17:37:42 -0300

    css on page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+
 import LogoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
+
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+import './styles.css'
 
 function Landing() {
     return (
@@ -16,8 +23,19 @@ function Landing() {
                     className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="" className=""></a>
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Estudar" />
+                        Estudar
+                    </a>
                 </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
             </div>
         </div>
     );
