04bb7e3ee78c79fdeb4182892262f2e21e90ad83
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-03 18:50:52 -0300

    create routes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -23,11 +23,11 @@ function Landing() {
                     className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <a href="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
                     </a>
-                    <a href="" className="give-classes">
+                    <a href="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Estudar" />
                         Dar aulas
                     </a>
