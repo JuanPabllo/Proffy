19723e9d076ebeadc04d29be4e7cf08d1a8faf88
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-03 18:16:50 -0300

    idented

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -7,7 +7,7 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-import './styles.css'
+import "./styles.css";
 
 function Landing() {
     return (
@@ -34,7 +34,8 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Total de 200 conexões já realizadas{" "}
+                    <img src={purpleHeartIcon} alt="Coração roxo" />
                 </span>
             </div>
         </div>
