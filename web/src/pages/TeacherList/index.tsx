114075e2fb7267d70015fcf3517edbac496fef88
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-03 19:16:43 -0300

    stykle of TearcherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+import './styles.css'
 
 function TeacherList() {
     return (
-        <h1>Teachers</h1>
-    )
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Voltar" />
+                    </Link>
+                    <img src={logoImg} alt="Logo Proffy" />
+                </div>
+
+                <div className="header-content">
+                    <strong>Esses são os proffys disponíveis.</strong>
+                </div>
+            </header>
+        </div>
+    );
 }
 
-
-export default TeacherList
+export default TeacherList;
