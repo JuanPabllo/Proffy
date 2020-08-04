18dd9c89c4356f8f0dae055cca444e5b480fda86
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-04 18:56:22 -0300

    try error database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -24,35 +24,45 @@ routes.post("/classes", async (request, response) => {
 
     const trx = await db.transaction();
 
-    const insertedUsersIds = await trx("users").insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    try {
+        const insertedUsersIds = await trx("users").insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
 
-    const user_id = insertedUsersIds[0];
+        const user_id = insertedUsersIds[0];
 
-    const insertedClassesIds = await trx("classes").insert({
-        subject,
-        cost,
-        user_id,
-    });
+        const insertedClassesIds = await trx("classes").insert({
+            subject,
+            cost,
+            user_id,
+        });
 
-    const class_id = insertedClassesIds[0];
+        const class_id = insertedClassesIds[0];
 
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHoursToMinutes(scheduleItem.from),
-            to: convertHoursToMinutes(scheduleItem.to),
-        };
-    });
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHoursToMinutes(scheduleItem.from),
+                to: convertHoursToMinutes(scheduleItem.to),
+            };
+        });
 
-    await trx("class_schedule").insert(classSchedule);
+        await trx("class_schedule").insert(classSchedule);
 
-    return response.send();
+        await trx.commit();
+
+        return response.status(201).send();
+    } catch (err) {
+        await trx.rollback();
+
+        return response.status(400).json({
+            error: "Unexpected error while creating new class",
+        });
+    }
 });
 
 export default routes;
