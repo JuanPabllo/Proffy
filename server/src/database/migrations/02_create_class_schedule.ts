801b4a4da57b1d77b06323968bf79efdf6e2439a
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-04 17:45:27 -0300

    datebase classes

---
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -0,0 +1,23 @@
+import knex from "knex";
+
+export async function up(knex: knex) {
+    return knex.schema.createTable("class_schedule", (table) => {
+        table.increments("id").primary();
+
+        table.integer("week_day").notNullable();
+        table.integer("from").notNullable();
+        table.integer("to").notNullable();
+
+        table
+            .integer("user_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+    });
+}
+
+export async function down(knex: knex) {
+    knex.schema.dropTable("class_schedule");
+}
