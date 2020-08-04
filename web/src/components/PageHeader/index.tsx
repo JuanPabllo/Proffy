88742b6aa7a05f7bb99693e0538057e05b9a9fa9
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-04 13:08:09 -0300

    style on user

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
 
-import './style.css';
+import "./style.css";
 
 interface PageHeaderProps {
     title?: string;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Pageheader from "../../components/PageHeader";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 import "./styles.css";
 
 function TeacherList() {
@@ -10,20 +12,52 @@ function TeacherList() {
                 <form id="search-teachers">
                     <div className="input-block">
                         <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
+                        <input type="text" id="subject" />
                     </div>
 
                     <div className="input-block">
                         <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
+                        <input type="text" id="week_day" />
                     </div>
 
                     <div className="input-block">
                         <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
+                        <input type="text" id="time" />
                     </div>
                 </form>
             </Pageheader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img
+                            src="https://avatars1.githubusercontent.com/u/59495901?s=460&u=78581ed6769f49a748a10da7cb03722587cc51be&v=4"
+                            alt="Juan Pablo"
+                        />
+                        <div>
+                            <strong>Juan Pablo</strong>
+                            <span>Física</span>
+                        </div>
+                    </header>
+                    <p>
+                        Entusiasta por tecnologias da física ahan
+                        <br />
+                        <br />
+                        Apaixonado em explodir coisas
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/Hora
+                            <strong>70,00</strong>
+                            <button type="button">
+                                <img src={whatsappIcon} alt="Whatsapp" />
+                                Entrar em contato
+                            </button>
+                        </p>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
