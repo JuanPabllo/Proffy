e11c54d4aff5b57f1150bda315a32a1d0f8039f8
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-06 19:13:07 -0300

    adding navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { AppLoading } from "expo";
+
 import {
   Archivo_400Regular,
   Archivo_700Bold,
@@ -11,7 +12,8 @@ import {
   Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
 
-import Landing from "./src/pages/Landing";
+
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -25,7 +27,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
