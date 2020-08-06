ca6f40bcae23927f8c95590d5cc482277046f9af
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-06 19:49:52 -0300

    adding text and styles of text

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { View } from 'react-native';
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+
+import styles from "./styles";
 
 function GiveClasses() {
-    return(
-        <View/>
-    )
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                resizeMode="contain"
+                source={giveClassesBgImage}
+                style={styles.content}
+            >
+                <Text style={styles.title}>Quer ser um Proffy?</Text>
+                <Text style={styles.description}>Para começar, você precisa se cadastrar como professor na nossa plataforma web.</Text>
+            </ImageBackground>
+        </View>
+    );
 }
 
 export default GiveClasses;
---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#8257e5",
+        justifyContent: "center",
+        padding: 40,
+    },
+
+    content: {
+        flex: 1,
+        justifyContent: "center",
+    },
+
+    title: {
+        fontFamily: "Archivo_700Bold",
+        color: "#FFF",
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180,
+    },
+
+    description: {
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240
+    },
+});
+export default styles;
