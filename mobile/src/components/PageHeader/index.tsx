6fe8c0a16822c5781fd10e809bf75d37c4248e19
JuanPabllo <juanpablo192.dreaw@gmail.com>
2020-08-07 16:12:30 -0300

    adding form

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { BorderlessButton } from "react-native-gesture-handler";
@@ -8,11 +8,16 @@ import logoImg from "../../assets/images/logo.png";
 
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode;
 }
 
 import styles from "./styles";
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+    title,
+    headerRight,
+    children,
+}) => {
     const { navigate } = useNavigation();
 
     function handleGoBack() {
@@ -29,7 +34,11 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
                 <Image source={logoImg} resizeMode="contain" />
             </View>
 
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+
+                {headerRight}
+            </View>
 
             {children}
         </View>
